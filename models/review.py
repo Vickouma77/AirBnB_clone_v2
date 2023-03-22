599b313e88dcbebdee22eddc3f664623ebe20d48
H3c7o4 <hectorvladitok@gmail.com>
2023-03-22 13:34:53 +0100

    updating the review class

--- models/review.py
+++ models/review.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+
+    __tablename__ = "reviews"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", cascade="all, delete", backref="cities")
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""

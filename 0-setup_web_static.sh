a177c1b6e0f78e66935acc03d665cc9c9799dc0c
vickouma77 <Vickprestone20@gmail.com>
2023-04-05 18:29:59 +0300

    setting up web server for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,19 @@
 #!/usr/bin/env bash
 #script that sets up your web servers for the deployment
 
-#installing nginx if not exist
-apt-get -y update
-apt-get -y install nginx
-
-#Creating static folders
+apt update -y
+apt install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-
 echo "<!DOCTYPE html>
-      <html>
-      <head>
-      </head>
-      <body>
-         <p>deployment test for vickouma.tech</p>
-      </body>
-      </html>" | tee /data/web_static/releases/test/index.html
-
-#creating a symbolic link
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-#change ownership
-chown ubuntu:ubuntu /data
-
-#configuring nginx
-sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
-
-service nginx restart
+chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo service nginx restart

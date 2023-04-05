be42c06fad0ac5f4a3a6d0715222086c62b80d46
vickouma77 <Vickprestone20@gmail.com>
2023-04-05 18:02:31 +0300

    setting up web server for deployment

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#script that sets up your web servers for the deployment
+
+#installing nginx if not exist
+apt-get -y update
+apt-get -y install nginx
+
+#Creating static folders
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+echo "<!DOCTYPE html>
+      <html>
+      <head>
+      </head>
+      <body>
+         <p>deployment test for vickouma.tech</p>
+      </body>
+      </html>" | tee /data/web_static/releases/test/index.html
+
+#creating a symbolic link
+rm -f /data/web_static/current && ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#change ownership
+chown ubuntu:ubuntu /data
+
+#configuring nginx
+sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+
+service nginx restart

50a3842c841d21ac5ba7b88490f715731592a11e
vickouma77 <Vickprestone20@gmail.com>
2023-04-22 00:42:00 +0300

    fixing indentation

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 @app.route('/c/<text', strict_slashes=False)
 def c_is_fun():
     """print c followed by the value of the text variable"""
-    return 'C {}'.format(text.replace('_', ' '))
+    return 'c {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':

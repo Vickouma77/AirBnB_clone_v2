77c22aaed4010efd74efc0e7954888b2f92effa0
vickouma77 <Vickprestone20@gmail.com>
2023-04-22 00:59:32 +0300

    adding extra blank line

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,6 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_hbnb():
     """returns Hello HBNB"""
@@ -25,4 +26,5 @@ def c_is_fun():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)

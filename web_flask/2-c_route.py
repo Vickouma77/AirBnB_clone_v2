67957f6c6ab39652740cbf98253dfb5334a2059b
vickouma77 <Vickprestone20@gmail.com>
2023-04-22 01:02:13 +0300

    adding extra blank line

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
-
 from flask import Flask
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
 def hello_hbnb():
-    """returns Hello HBNB"""
+    """print web"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """returns HBNB"""
+    """print web"""
     return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c_is_fun():
-    """print c followed by the value of the text variable"""
-    return f'c {text.replace("_", " ")}'
+def c_is_fun(text):
+    """print C followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':

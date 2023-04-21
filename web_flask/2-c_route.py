e3da429f3e5bab16064503f690e85c72c4994212
vickouma77 <Vickprestone20@gmail.com>
2023-04-22 00:45:02 +0300

    fixing indentation

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,21 +4,21 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """returns Hello HBNB"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     """returns HBNB"""
     return "HBNB"
 
 
-@app.route('/c/<text', strict_slashes=False)
+@app.route('/c/<text')
 def c_is_fun():
     """print c followed by the value of the text variable"""
     return 'c {}'.format(text.replace('_', ' '))
